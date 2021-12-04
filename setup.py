e5808ba4abc09fd89ff37c14cb7c444a8a70c902
Edward-Stables <edward.stables@gmail.com>
2021-12-04 14:04:18 +0000

    Add regression

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from argparse import ArgumentParser
+from argparse import ArgumentError, ArgumentParser
 from requests import get
 from datetime import datetime as dt
-from os import mkdir, chmod
+from os import mkdir, chmod, listdir
 from os.path import isdir, isfile, join
 from subprocess import Popen
 
@@ -12,12 +12,13 @@ def get_session_id():
 
 def get_args():
     parser = ArgumentParser()
-    parser.add_argument("--day", "-d", required=True, type=int, help="What day to setup")
+    parser.add_argument("--day", "-d", type=int, help="What day to setup")
     parser.add_argument("--build", "-b", action="store_true", help="Create day template")
     parser.add_argument("--problem", "-p", action="store_true", help="Open the problem statement in the browser")
     parser.add_argument("--leaderboard", "-l", action="store_true", help="Open the leaderboard in the browser")
     parser.add_argument("--year", "-y", type=int, default=dt.now().year)
     parser.add_argument("--run", "-r", action="store_true", help="Run the given day and print answer + timing info (exc data loading)")
+    parser.add_argument("--regression", action="store_true", help="Like --run, but runs all days")
     return parser.parse_args()
 
 def get_data(day: int, year: int, session_id: str):
@@ -75,21 +76,38 @@ def run(day):
     b_res = module.task.main_b(data) 
     b_time = (time() - t1) * 1000
 
-    print(f"Day {day}")
     print(f"a: {a_time:07.3f}ms  {a_res}")
     print(f"b: {b_time:07.3f}ms  {b_res}")
+
+def run_day(day):
+    print(f"Day {day}")
+    run(day)
+
+def run_regression():
+    days = [int(d.split("_")[1]) for d in listdir(".") if d.startswith("day")]
+    days.sort()
+    for d in days:
+        run_day(d)
 
 def main():
     args = get_args()
     if args.problem:
+        if args.day == None:
+            raise ArgumentError("Argument 'problem' requires --day N argument")
         Popen(f"$BROWSER https://adventofcode.com/{args.year}/day/{args.day}", shell=True)        
     if args.leaderboard:
         Popen(f"$BROWSER https://adventofcode.com/{args.year}/leaderboard", shell=True)        
     if args.build:
+        if args.day == None:
+            raise ArgumentError("Argument 'build' requires --day N argument")
         data = get_data(args.day, args.year, get_session_id())
         create_template(args.day, data)
     if args.run:
+        if args.day == None:
+            raise ArgumentError("Argument 'run' requires --day N argument")
         run(args.day)
+    if args.regression:
+        run_regression()
 
 if __name__ == "__main__":
     main()
