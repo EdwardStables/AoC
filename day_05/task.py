6d92e66441f10f9ce39dcfd5fa559a348f3252ce
Edward-Stables <edward.stables@gmail.com>
2021-12-05 13:58:32 +0000

    Clean up and add list comprehensions

--- day_05/task.py
+++ day_05/task.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from timeit import default_timer as dt
 
 def get_data(fname = "data.txt"):
     with open(f"day_05/{fname}") as f:
@@ -7,11 +8,9 @@ def get_data(fname = "data.txt"):
 def get_points(data):
     s,e = data.split(" -> ")
     s = s.split(",")
-    sx = int(s[0])
-    sy = int(s[1])
+    sx, sy = int(s[0]), int(s[1])
     e = e.split(",")
-    ex = int(e[0])
-    ey = int(e[1])
+    ex, ey = int(e[0]), int(e[1])
     return sx,sy,ex,ey
 
 def interpolate(sx,sy,ex,ey):
@@ -23,63 +22,39 @@ def interpolate(sx,sy,ex,ey):
 def interpolate_d(sx,sy,ex,ey):
     x_dir = 1 if ex > sx else -1
     y_dir = 1 if ey > sy else -1
-    x = sx
-    y = sy
-    points = []
-    for _ in range(abs(ey-sy) + 1):
-        points.append((x,y))
-        x += x_dir
-        y += y_dir
-
-    return points
+    return [p for p in zip(range(sx,ex+x_dir,x_dir),range(sy,ey+y_dir,y_dir))]
 
 def interpolate_hv(sx,sy,ex,ey):
-    points = []
     if sx == ex: #vert
-        y1 = min(sy, ey)
-        y2 = max(sy, ey)
-        for y in range(y1, y2+1):
-            points.append((sx,y))
+        y_dir = 1 if ey > sy else -1
+        return [(sx,y) for y in range(sy, ey+y_dir,y_dir)]
     elif sy == ey:
-        x1 = min(sx, ex)
-        x2 = max(sx, ex)
-        for x in range(x1, x2+1):
-            points.append((x,sy))
+        x_dir = 1 if ex > sx else -1
+        return [(x,sy) for x in range(sx, ex+x_dir,x_dir)]
+    else:
+        return []
 
-    return points
+def main(data, interp_func):
+    point_list = {}
+    high_count = 0
+    for d in data:
+        start_end = get_points(d)
+        actual_points = interp_func(*start_end)
+        for p in actual_points:
+            if p in point_list:
+                if point_list[p] == 1:
+                    point_list[p] = 2
+                    high_count += 1
+
+            else:
+                point_list[p] = 1
+    return high_count
 
 def main_a(data):
-    point_list = {}
-    high_count = 0
-    for d in data:
-        start_end = get_points(d)
-        actual_points = interpolate_hv(*start_end)
-        for p in actual_points:
-            if p in point_list:
-                if point_list[p] == 1:
-                    point_list[p] = 2
-                    high_count += 1
-
-            else:
-                point_list[p] = 1
-    return high_count
+    return main(data, interpolate_hv)
 
 def main_b(data):
-    point_list = {}
-    high_count = 0
-    for d in data:
-        start_end = get_points(d)
-        actual_points = interpolate(*start_end)
-        for p in actual_points:
-            if p in point_list:
-                if point_list[p] == 1:
-                    point_list[p] = 2
-                    high_count += 1
-
-            else:
-                point_list[p] = 1
-
-    return high_count
+    return main(data, interpolate)
 
 if __name__ == "__main__":
     data = get_data()
