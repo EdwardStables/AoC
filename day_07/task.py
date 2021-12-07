b47c5935d2e85a404f2198e712c328ba2f779c9c
Edward Stables <edward.stables1198@gmail.com>
2021-12-07 10:41:33 +0000

    Day 7 BAD

---
+++ day_07/task.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+from math import floor, ceil
+def get_data(fname = "data.txt"):
+    with open(f"day_07/{fname}") as f:
+        return [l.strip() for l in f]
+
+def get_fuel(data, pos):
+    return sum(abs(pos-d) for d in data)
+
+def get_fuel_exp(data, pos):
+    s = 0
+    for d in data:
+        s += tri(pos-d)
+    return s
+
+def tri(n):
+    n = abs(n)
+    t = 0
+    for i in range(1, n+1):
+        t += i
+    return t
+
+def main(data,fuel_func):
+    h = [int(d) for d in data[0].split(",")]
+    start = min(h)
+    stop = max(h)
+    min_fuel = fuel_func(h, start)
+    for f in range(start+1, stop+1):
+        if (new_min := fuel_func(h, f)) < min_fuel:
+            min_fuel = new_min 
+
+    return min_fuel
+
+def main_a(data):
+    return main(data, get_fuel)
+
+def main_b(data):
+    return main(data, get_fuel_exp)
+
+if __name__ == "__main__":
+    data = get_data()
+    print(main_a(data))
+    data = get_data()
+    print(main_b(data))
