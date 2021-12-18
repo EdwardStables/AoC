ccc17940ec1ccec15b13ba33a1e331cab27b32b1
Edward Stables <edward.stables1198@gmail.com>
2021-12-18 21:49:30 +0000

    Binary tree

--- day_18/task.py
+++ day_18/task.py
@@ -6,104 +6,147 @@ def get_data(fname = "data.txt"):
     with open(f"day_18/{fname}") as f:
         return [l.strip() for l in f]
 
+class Node:
+    def __init__(self, left, right):
+        self.l = left
+        self.r = right
+
+def treeify(arr):
+    if type(arr[0]) == type(arr[1]) == int:
+        return Node(arr[0], arr[1])
+    if type(arr[0]) == int:
+        return Node(arr[0], treeify(arr[1]))
+    if type(arr[1]) == int:
+        return Node(treeify(arr[0]), arr[1])
+    return Node(treeify(arr[0]), treeify(arr[1]))
+
 def parse(data):
-    return [eval(d) for d in data]
+    return [treeify(eval(d)) for d in data]
 
-def explode(num):
-    return False if explode_rec(num, 1) == False else True
+def explode(n, depth):
+    if depth == 4:
+        cl = None
+        cr = None
+        exp = False
+        if type(n.l) != int:
+            add_to_left_of_right(n, n.l.r)
+            cl = n.l.l
+            n.l = 0
+            exp = True
+        elif type(n.r) != int:
+            add_to_right_of_left(n, n.r.l)
+            cr = n.r.r
+            n.r = 0
+            exp = True
+        return cl, cr, exp
 
-def add_next(num, to_add):
-    if type(num[0]) == int:
-        num[0] += to_add
+    fcl = None
+    fcr = None
+    fexp = False 
+    if type(n.l) != int:
+        cl, cr, exp = explode(n.l, depth+1)
+        if cr:
+            add_to_left_of_right(n, cr)
+        fcl = cl
+        fexp |= exp
+    if not fexp and type(n.r) != int:
+        cl, cr, exp = explode(n.r, depth+1)
+        if cl:
+            add_to_right_of_left(n, cl)
+        fcr = cr 
+        fexp |= exp
+
+    return fcl, fcr, fexp
+
+def add_to_left_of_right(n, ta):
+    if type(n.r) == int:
+        n.r += ta
     else:
-        add_next(num[0], to_add)
+        add_to_left(n.r, ta)
 
-def add_prev(num, to_add):
-    if type(num[-1]) == int:
-        num[-1] += to_add
+def add_to_right_of_left(n, ta):
+    if type(n.l) == int:
+        n.l += ta
     else:
-        add_prev(num[-1], to_add)
+        add_to_right(n.l, ta)
 
-def explode_rec(num, depth):
-    if depth == 5:
-        a, b = num
-        return a, b, True
+def add_to_right(n, ta):
+    if type(n.r) == int:
+        n.r += ta
     else:
-        for i, n in enumerate(num):
-            if type(n) == list:
-                res = explode_rec(n, depth+1)
-                if res == False:
-                    continue
-                a = res[0]
-                b = res[1]
-                set_0 = res[2]
-                if i > 0:
-                    if type(num[i-1]) == int:
-                        num[i-1] += a
-                    else:
-                        add_prev(num[i-1], a)
-                    a = 0
-                if i < len(num) - 1:
-                    if type(num[i+1]) == int:
-                        num[i+1] += b
-                    else:
-                        add_next(num[i+1], b)
-                    b = 0
-                if set_0:
-                    num[i] = 0
-                return a, b, False
-    return False
+        add_to_right(n.r, ta)
+    
+def add_to_left(n, ta):
+    if type(n.l) == int:
+        n.l += ta
+    else:
+        add_to_left(n.l, ta)
 
-def split(num):
-    for i, n in enumerate(num):
-        if type(n) == list:
-            if split(n):
-                return True
-        else:
-            if n > 9:
-                num[i] = [floor(n/2), ceil(n/2)]
-                return True
-    return False
+def split(n):
+    if type(n.l) == int:
+        if n.l > 9:
+            n.l = Node(floor(n.l/2), ceil(n.l/2)) 
+            return True
+    else:
+        if split(n.l):
+            return True
+    if type(n.r) == int:
+        if n.r > 9:
+            n.r = Node(floor(n.r/2), ceil(n.r/2)) 
+            return True
+    else:
+        if split(n.r):
+            return True
+    
 
-def magnitude(num):
-    a = num[0]
-    b = num[1]
-    if type(a) != int:
-        a = magnitude(a)
-    if type(b) != int:
-        b = magnitude(b)
-    return 3*a + 2*b
+def magnitude(n):
+    if type(n) == int:
+        return n
+    return 3*magnitude(n.l) + 2*magnitude(n.r)
 
 def reduce(num):
     while True:
-        if explode(num):
-            continue
+        _, _, exp = explode(num, 1)
+        if exp:
+            continue        
         if split(num):
             continue
         break
     return num
 
+def leafs(n, end=True):
+    if type(n.l) == int:
+        print(n.l, end=" ")
+    else:
+        leafs(n.l, end=False)
+    if type(n.r) == int:
+        print(n.r, end=" ")
+    else:
+        leafs(n.r, end=False)
+
+    if end:
+        print()
+    
 def main_a(data):
     data = parse(data)
     active = data[0]
     next = 1
-    
     while next < len(data):
-        active = reduce([active, data[next]])
+        active = Node(active, data[next])
+        active = reduce(active)
         next += 1
 
     return magnitude(active)
 
 def main_b(data):
-    data = parse(data)
+    arrs = [eval(d) for d in data]
     max_mag = 0
 
     for i in range(len(data)):
         for j in range(len(data)):
             if i == j:
                 continue
-            max_mag = max(max_mag, magnitude(reduce([deepcopy(data[i]), deepcopy(data[j])])))
-        
+            max_mag = max(max_mag, magnitude(reduce(Node(treeify(arrs[i]), treeify(arrs[j])))))
 
     return max_mag 
 
@@ -112,3 +155,7 @@ if __name__ == "__main__":
     print(main_a(data))
     data = get_data()
     print(main_b(data))
+
+
+
+
