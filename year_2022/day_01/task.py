72fd2c3fff7d0882238631940d4843c2b0f421fc
Edward Stables <edward.stables1198@gmail.com>
2022-12-01 10:37:24 +0000

    2022 day 1

---
+++ year_2022/day_01/task.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+def get_data(fname = "data.txt"):
+    with open(f"year_2022/day_01/{fname}") as f:
+        return [l.strip() for l in f]
+
+def get_sums(data):
+    sums = [0]
+
+    for line in data:
+        if line == "":
+            sums.append(0)
+        else:
+            sums[-1] += int(line)
+    return sums
+
+def main_a(data):
+    return max(get_sums(data)) 
+
+def main_b(data):
+    sums = sorted(get_sums(data))
+    return sums[-1] + sums[-2] + sums[-3]
+
+if __name__ == "__main__":
+    data = get_data()
+    print(main_a(data))
+    data = get_data()
+    print(main_b(data))
