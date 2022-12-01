73e2180c5d68a655fcbf26d97ecf39261e3d1c88
Edward Stables <edward.stables1198@gmail.com>
2022-12-01 13:45:07 +0000

    move int conv somewhere else

--- year_2022/day_01/task.py
+++ year_2022/day_01/task.py
@@ -2,18 +2,18 @@
 
 def get_data(fname = "data.txt"):
     with open(f"year_2022/day_01/{fname}") as f:
-        return [l.strip() for l in f]
+        return [None if l.strip() == "" else int(l.strip()) for l in f]
 
 def main_a(data):
     ma = 0 
     cur = 0
     for line in data:
-        if line == "":
+        if line is None:
             if cur > ma:
                 ma = cur 
             cur = 0 
         else:
-            cur += int(line)
+            cur += line
     return ma
 
 def main_b(data):
@@ -22,7 +22,7 @@ def main_b(data):
     m3 = 0 
     cur = 0
     for line in data:
-        if line == "":
+        if line is None:
             if cur > m3:
                 m1 = m2
                 m2 = m3
@@ -34,7 +34,7 @@ def main_b(data):
                 m1 = cur
             cur = 0 
         else:
-            cur += int(line)
+            cur += line
     return m1+m2+m3
 
 if __name__ == "__main__":
