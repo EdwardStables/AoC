1fec0134a3e937ed5c9d07ee5853052765f6db06
Edward Stables <edward.stables1198@gmail.com>
2022-12-06 11:51:36 +0000

    window based, slower

--- year_2022/day_06/task.py
+++ year_2022/day_06/task.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python3
+from collections import defaultdict
 
 def get_data(fname = "data.txt"):
     with open(f"year_2022/day_06/{fname}") as f:
         return [l.strip() for l in f][0]
 
+def run(data, size):
+    window = defaultdict(int)
+    for d in data[:size]:
+        window[d] += 1
+
+    for i, d in enumerate(data[size:]):
+        window[data[i]] -= 1
+        window[d] += 1
+        if all(v in (1,0) for v in window.values()):
+            return i+size+1
+
 def main_a(data):
-    for i in range(len(data)-3):
-        s = set(data[i:i+4])
-        if len(s) == 4:
-            return i+4
+    return run(data, 4)
 
 def main_b(data):
-    for i in range(len(data)-13):
-        s = set(data[i:i+14])
-        if len(s) == 14:
-            return i+14
-    return 0
+    return run(data, 14)
 
 if __name__ == "__main__":
     data = get_data()
