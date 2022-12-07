9eac7ab96588cd265c9baf071f2a030537dbe3b0
Edward Stables <edward.stables1198@gmail.com>
2022-12-07 08:42:05 +0000

    technically faster d6

--- year_2022/day_06/task.py
+++ year_2022/day_06/task.py
@@ -6,6 +6,29 @@ def get_data(fname = "data.txt"):
         return [l.strip() for l in f][0]
 
 def run(data, size):
+    latest = size-1
+    while True:
+        window = data[latest-size+1:latest+1]
+        bf = False
+        for i, d1 in enumerate(window):
+            for j, d2 in enumerate(window):
+                if i == j:
+                    continue
+                if d1 == d2:
+                    latest += min(i, j) + 1
+                    bf = True
+                    break
+            if bf:
+                break
+        if bf:
+            continue 
+        return latest+1
+
+
+def main_a(data):
+    return run(data, 4)
+
+def run_dict_window(data, size):
     window = defaultdict(int)
     for d in data[:size]:
         window[d] += 1
@@ -16,11 +39,8 @@ def run(data, size):
         if all(v in (1,0) for v in window.values()):
             return i+size+1
 
-def main_a(data):
-    return run(data, 4)
-
 def main_b(data):
-    return run(data, 14)
+    return run_dict_window(data, 14)
 
 if __name__ == "__main__":
     data = get_data()
