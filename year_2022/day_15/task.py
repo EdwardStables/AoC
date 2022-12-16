bc94160deb51b32a90bab3f6066a2f406b7b5592
Edward Stables <edward.stables1198@gmail.com>
2022-12-16 00:12:04 +0000

    pt2

--- year_2022/day_15/task.py
+++ year_2022/day_15/task.py
@@ -14,7 +14,6 @@ def main_a(data):
         sensors.append((int(l[2][2:-1]), int(l[3][2:-1])))
         beacons.append((int(l[8][2:-1]), int(l[9][2:])))
 
-        
     ranges = []
     for s,b in zip(sensors, beacons):
         x_diff = b[0] - s[0]
@@ -35,7 +34,6 @@ def main_a(data):
         else:
             cur_ind += 1
 
-    sensors_on_target = set(s for s in sensors if s[1] == target)
     beacons_on_target = set(b for b in beacons if b[1] == target)
 
     to_add = 0
@@ -47,7 +45,68 @@ def main_a(data):
     return sum(u-l+1 for l,u in ranges)+to_add
 
 def main_b(data):
-    return 0
+    max_coord = 4000000 if len(data)==23 else 20
+    sensors = []
+    beacons = []
+    for line in data:
+        l = line.split()
+        sensors.append((int(l[2][2:-1]), int(l[3][2:-1])))
+        beacons.append((int(l[8][2:-1]), int(l[9][2:])))
+
+        
+    for l in range(0, max_coord):
+        ranges = []
+        for s,b in zip(sensors, beacons):
+            x_diff = b[0] - s[0]
+            y_diff = b[1] - s[1]
+            diff = abs(x_diff) + abs(y_diff)
+            x_width = diff - abs(s[1]-l)
+            if x_width <= 0:
+                continue
+            ranges.append((s[0]-x_width, s[0]+x_width))
+
+        ranges.sort()
+
+        cur_ind = 0
+        while cur_ind < len(ranges)-1:
+            if ranges[cur_ind][1] >= ranges[cur_ind+1][0]:
+                if ranges[cur_ind+1][1] > ranges[cur_ind][1]:
+                    ranges[cur_ind] = (ranges[cur_ind][0], ranges[cur_ind+1][1])
+                ranges.pop(cur_ind+1)
+            else:
+                cur_ind += 1
+
+        cur_ind = 0
+        while cur_ind < len(ranges):
+            if ranges[cur_ind][1] < 0:
+                ranges.pop(cur_ind)
+                continue
+            elif ranges[cur_ind][0] < 0:
+                ranges[cur_ind] = (0, ranges[cur_ind][1])
+            else:
+                break
+            cur_ind += 1
+
+        cur_ind = len(ranges)-1
+        while cur_ind >= 0:
+            if ranges[cur_ind][0] > max_coord:
+                ranges.pop(cur_ind)
+                continue
+            elif ranges[cur_ind][1] > max_coord:
+                ranges[cur_ind] = (ranges[cur_ind][0], max_coord)
+            else:
+                break
+            cur_ind -= 1
+
+
+        spaces = sum(u-l+1 for l,u in ranges)
+        if spaces == max_coord+1:
+            continue
+        for i in range(len(ranges)-1):
+            if ranges[i+1][0] > ranges[i][1]+1:
+                return (ranges[i][1]+1) * 4000000 + l
+            
+    return 
 
 if __name__ == "__main__":
     data = get_data()
