0b172db763ed669c165c82d305a3b377cbb66d6a
Edward Stables <edward.stables1198@gmail.com>
2023-12-01 16:32:18 +0000

    remove dedicated functions

--- year_2023/day_01/task.py
+++ year_2023/day_01/task.py
@@ -8,42 +8,16 @@ def main_a(data):
     sum = 0
 
     for line in data:
-        n = ""
         for char in line:
-            if char.isnumeric():
-                n += char
-        sum += int(n[0] + n[-1])
+            if char in "0123456789":
+                sum += 10*int(char)
+                break
+        for char in line[::-1]:
+            if char in "0123456789":
+                sum += int(char)
+                break
         
     return sum
-
-def fwd_search(s):
-    if s[0] not in "otfsen": return None
-    if s[0:3] == "one": return 1
-    if s[0:3] == "two": return 2
-    if s[0:5] == "three": return 3
-    if s[0:4] == "four": return 4
-    if s[0:4] == "five": return 5
-    if s[0:3] == "six": return 6
-    if s[0:5] == "seven": return 7
-    if s[0:5] == "eight": return 8
-    if s[0:4] == "nine": return 9
-
-    return None
-
-def bwd_search(s):
-    if s[-1] not in "eorxnt": return None
-    if s[-1] == "e":
-        if s[-3:] == "one": return 1
-        if s[-5:] == "three": return 3
-        if s[-4:] == "five": return 5
-        if s[-4:] == "nine": return 9
-    if s[-3:] == "two": return 2
-    if s[-4:] == "four": return 4
-    if s[-3:] == "six": return 6
-    if s[-5:] == "seven": return 7
-    if s[-5:] == "eight": return 8
-
-    return None
 
 def main_b(data):
     sum = 0
@@ -51,10 +25,20 @@ def main_b(data):
     for line in data:
         for i, char in enumerate(line):
             found = None
-            if char.isnumeric():
+            if char in "0123456789":
                 found = int(char)
-            else:
-                found = fwd_search(line[i:])
+            elif char in "otfsen":
+                s = line[i:]
+                if s[0:3] == "one": found = 1
+                elif s[0:3] == "two": found = 2
+                elif s[0:5] == "three": found = 3
+                elif s[0:4] == "four": found = 4
+                elif s[0:4] == "five": found = 5
+                elif s[0:3] == "six": found = 6
+                elif s[0:5] == "seven": found = 7
+                elif s[0:5] == "eight": found = 8
+                elif s[0:4] == "nine": found = 9
+
             if found is None: continue
 
             sum += 10*int(found)
@@ -62,10 +46,20 @@ def main_b(data):
 
         for i, char in enumerate(line[::-1]):
             found = None
-            if char.isnumeric():
+            if char in "0123456789":
                 found = int(char)
-            else:
-                found = bwd_search(line[:len(line)-i])
+            elif char in "eorxnt":
+                s = line[:len(line)-i]
+                if s[-1] == "e":
+                    if s[-3:] == "one": found = 1
+                    elif s[-5:] == "three": found = 3
+                    elif s[-4:] == "five": found = 5
+                    elif s[-4:] == "nine": found = 9
+                elif s[-3:] == "two": found = 2
+                elif s[-4:] == "four": found = 4
+                elif s[-3:] == "six": found = 6
+                elif s[-5:] == "seven": found = 7
+                elif s[-5:] == "eight": found = 8
             if found is None: continue
 
             sum += int(found)
