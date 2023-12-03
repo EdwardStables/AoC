aa6c2ae42cf64a322b27b3b7ab1e9d5433a4908f
Edward Stables <edward.stables1198@gmail.com>
2023-12-03 12:32:21 +0000

    d3

--- year_2023/day_03/task.py
+++ year_2023/day_03/task.py
@@ -42,8 +42,69 @@ def main_a(data):
             sum += res
     return sum
 
-def main_b(data):
+def getnum(data,x):
+    num = data[x]
+    _x = x-1
+    while _x >= 0 and data[_x].isnumeric():
+        num = data[_x] + num
+        _x -= 1
+    _x = x+1
+    while _x < len(data) and data[_x].isnumeric():
+        num = num + data[_x]
+        _x += 1
+
+    return int(num)
+
+
+def get_ratio(data, y, x):
+    n = []
+    if x > 0:
+        if data[y][x-1].isnumeric():
+            res = getnum(data[y],x-1)
+            n.append(res)
+    if x < len(data[0])-1:
+        if data[y][x+1].isnumeric():
+            res = getnum(data[y],x+1)
+            n.append(res)
+
+    if y > 0:
+        #check middle
+        if not data[y-1][x].isnumeric():
+            if data[y-1][x-1].isnumeric():
+                res = getnum(data[y-1],x-1)
+                n.append(res)
+            if data[y-1][x+1].isnumeric():
+                res = getnum(data[y-1],x+1)
+                n.append(res)
+        else:
+            res = getnum(data[y-1],x)
+            n.append(res)
+
+    if y < len(data)-1:
+        #check middle
+        if not data[y+1][x].isnumeric():
+            if data[y+1][x-1].isnumeric():
+                res = getnum(data[y+1],x-1)
+                n.append(res)
+            if data[y+1][x+1].isnumeric():
+                res = getnum(data[y+1],x+1)
+                n.append(res)
+        else:
+            res = getnum(data[y+1],x)
+            n.append(res)
+
+    if len(n) == 2:
+        return n[0] * n[1]
     return 0
+
+def main_b(data):
+    sum = 0
+    for y, line in enumerate(data):
+        for x, item in enumerate(line):
+            if item == "*":
+                sum += get_ratio(data,y,x)
+
+    return sum
 
 if __name__ == "__main__":
     data = get_data()
