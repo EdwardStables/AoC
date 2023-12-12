92afcc8c08fa34d465a15fde9d8d94f16d0f7f35
Edward Stables <edward.stables1198@gmail.com>
2023-12-12 21:02:40 +0000

    speedups, remove dead code

--- year_2023/day_12/task.py
+++ year_2023/day_12/task.py
@@ -6,28 +6,10 @@ def get_data(fname = "data.txt"):
     with open(f"year_2023/day_12/{fname}") as f:
         return [l.strip() for l in f]
 
-def cache_call(pattern: str, remaining: tuple[int], in_pattern, cache: dict[str,dict[tuple[int],int]]):
-    cache_result = None
-    if pattern in cache and remaining in cache[pattern] and int(in_pattern) in cache[pattern][remaining]:
-        cache_result = cache[pattern][remaining][int(in_pattern)]
-        return cache_result
-
-    result = run_pattern(pattern, remaining, in_pattern, cache)
-    if pattern not in cache:
-        cache[pattern] = {}
-    if remaining not in cache[pattern]:
-        cache[pattern][remaining] = {}
-    cache[pattern][remaining][int(in_pattern)] = result
-
-    return result
-
 @cache
 def run_pattern(pattern: str, remaining: tuple[int], in_pattern):
     if len(pattern) == 0:
-        if len(remaining) == 0 or len(remaining) == 1 and remaining[0] == 0:
-            return 1
-        else:
-            return 0
+        return int(len(remaining) == 0 or len(remaining) == 1 and remaining[0] == 0)
 
     if in_pattern:
         if len(remaining) > 0 and remaining[0] != 0:
@@ -36,21 +18,16 @@ def run_pattern(pattern: str, remaining: tuple[int], in_pattern):
 
     if pattern[0] == ".":
         if len(remaining) > 0 and remaining[0] == 0:
-            nr = tuple(r for r in remaining[1:])
-        else:
-            nr = tuple(r for r in remaining)
-        return run_pattern(pattern[1:], nr, False)
+            remaining = remaining[1:]
+        return run_pattern(pattern[1:], remaining, False)
     
     if pattern[0] == "#":
-        if len(remaining) == 0: return 0
-        if remaining[0] == 0: return 0
-        nr = (remaining[0]-1,) + tuple(r for r in remaining[1:])
-        return run_pattern(pattern[1:], nr, True)
+        if len(remaining) == 0 or remaining[0] == 0: return 0
+        return run_pattern(pattern[1:], (remaining[0]-1,) + remaining[1:], True)
 
     if pattern[0] == "?":
-        a = run_pattern("#" + pattern[1:], remaining, in_pattern)
-        b = run_pattern("." + pattern[1:], remaining, in_pattern)
-        return a + b
+        return run_pattern("#" + pattern[1:], remaining, in_pattern) +\
+               run_pattern("." + pattern[1:], remaining, in_pattern)
 
     assert False
 
