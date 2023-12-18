9c3ac1300dde37d1b9a165baeffb53cfebf9fa87
Edward Stables <edward.stables1198@gmail.com>
2023-12-18 15:53:23 +0000

    both parts and clean

--- year_2023/day_18/task.py
+++ year_2023/day_18/task.py
@@ -5,69 +5,80 @@ def get_data(fname = "data.txt"):
         return [l.strip() for l in f]
 
 def main_a(data):
-    current = (0,0)
-    edges = set([current])
-    minx = 0
-    miny = 0
-    maxx = 0
-    maxy = 0
-    for line in data:
+    edges = []
+    last_pos = (0,0)
+    h_lines = []    
+
+    for i, line in enumerate(data):
         d, l, _ = line.split()
         l = int(l)
-        for _ in range(l):
-            if d == "R":
-                current = (current[0]+1,current[1])
-            if d == "L":
-                current = (current[0]-1,current[1])
-            if d == "U":
-                current = (current[0],current[1]+1)
-            if d == "D":
-                current = (current[0],current[1]-1)
-            if current[0] < minx:
-                minx = current[0]
-            if current[0] > maxx:
-                maxx = current[0]
-            if current[1] < miny:
-                miny = current[1]
-            if current[1] > maxy:
-                maxy = current[1]
-
-            edges.add(current)
-
-    for y in range(maxy, miny-1, -1):
-        for x in range(minx, maxx-1):
-            if (x, y) not in edges and \
-               (x+1, y) in edges and \
-               (x+2, y) not in edges:
-                ff_start = (x+2, y)
-                break
+        if d == "R":
+            h_lines.append(i)
+            endpos = (last_pos[0]+l,last_pos[1])
+        elif d == "L":
+            h_lines.append(i)
+            endpos = (last_pos[0]-l,last_pos[1])
+        elif d == "U":
+            endpos = (last_pos[0],last_pos[1]+l)
+        elif d == "D":
+            endpos = (last_pos[0],last_pos[1]-l)
         else:
-            continue
-        break
-
-    inside = set([ff_start])
-    ff_queue = [ff_start]
-
-    while len(ff_queue):
-        f = ff_queue.pop(0)
-
-        next = [
-            (f[0]+1,f[1]),
-            (f[0]-1,f[1]),
-            (f[0],f[1]+1),
-            (f[0],f[1]-1)
-        ]
-
-        for n in next:
-            if n in inside or n in edges:
-                continue
-            inside.add(n)
-            ff_queue.append(n)
-
-    return len(edges) + len(inside)
+            assert False, (d, type(d))
+            
+        edges.append((last_pos, endpos))
+        last_pos = endpos
+    
+    return area(edges, h_lines)
 
 def main_b(data):
-    return 0
+    edges = []
+    last_pos = (0,0)
+    h_lines = []    
+
+    for i, line in enumerate(data):
+        hex = line.split()[-1][2:-1]
+        l = int(hex[:-1],base=16)
+        d = hex[-1]
+        if d == "0":
+            h_lines.append(i)
+            endpos = (last_pos[0]+l,last_pos[1])
+        elif d == "2":
+            h_lines.append(i)
+            endpos = (last_pos[0]-l,last_pos[1])
+        elif d == "3":
+            endpos = (last_pos[0],last_pos[1]+l)
+        elif d == "1":
+            endpos = (last_pos[0],last_pos[1]-l)
+        else:
+            assert False, (d, type(d))
+            
+        edges.append((last_pos, endpos))
+        last_pos = endpos
+    
+    return area(edges,h_lines)
+    
+def area(edges, h_lines):
+    sum = 0
+    for i in h_lines:
+        width = (edges[i][1][0] - edges[i][0][0])
+        depth = edges[i][0][1]
+        if width > 0:
+            width += 1
+            depth += 1
+            if edges[i-1][0][1] > edges[i][0][1]:
+                width -= 1
+            if edges[i+1][1][1] > edges[i][1][1]:
+                width -= 1
+        else:
+            width -= 1
+            if edges[i-1][0][1] < edges[i][0][1]:
+                width += 1
+            if edges[i+1][1][1] < edges[i][1][1]:
+                width += 1
+
+        sum += width * depth
+         
+    return sum
 
 if __name__ == "__main__":
     data = get_data()
