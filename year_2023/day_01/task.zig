f7aa447f47f50de8eae624fe25f3794fc872512d
Edward Stables <edward.stables1198@gmail.com>
2024-01-01 17:42:39 +0000

    zig 2023 d1p1

--- year_2023/day_01/task.zig
+++ year_2023/day_01/task.zig
@@ -1,11 +1,28 @@
 const std = @import("std");
 
 pub fn task_a(data: [][]u8, len: u16) u64 {
-    std.debug.print("{s}\n", .{data[len - 2]});
-    return 1;
+    var total: u32 = 0;
+    var i: u16 = 0;
+    while (i < len) : (i += 1) {
+        var on_first = true;
+        var local: u32 = 0;
+        for (data[i]) |c| {
+            if (c < 48 or c > 57) continue;
+            if (on_first) {
+                local = (c - 48);
+                total += 10 * local;
+                on_first = false;
+            } else {
+                local = (c - 48);
+            }
+        }
+        total += local;
+    }
+    return total;
 }
 
 pub fn task_b(data: [][]u8, len: u16) u64 {
-    std.debug.print("{s}\n", .{data[len - 2]});
+    _ = data;
+    _ = len;
     return 2;
 }
