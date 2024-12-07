9b2fabcd1e728712a8164ca103da879d883e6eb7
Edward Stables <edward.stables1198@gmail.com>
2024-12-07 22:34:29 +0000

    recursive implementation

--- year_2024/day_07/task.zig
+++ year_2024/day_07/task.zig
@@ -21,50 +21,46 @@ pub fn task1(_: Allocator, input: *std.ArrayList([]const u8)) aoc.TaskErrors!i64
         }
 
         ind -= 2; //Length of local array
-
-        const dif_bits = ind - 1;
-        var combo_ind: u16 = 0;
-        while (combo_ind < std.math.pow(u16, 2, dif_bits)) : (combo_ind+=1) {
-            var result = factors[0];
-
-            for (0..dif_bits) |i| {
-                const mult: bool = (combo_ind & (@as(u16, 1) << @as(u4, @intCast(i)))) > 0;
-                if (mult) {
-                    result *= factors[i+1];
-                } else {
-                    result += factors[i+1];
-                }
-            }
-
-            if (result == target) {
-                total += target;
-                break;
-            }
-        }
+        if (calcNextP1(factors[1..ind], factors[0], target)) total += target;
     }
 
 
     return total;
 }
 
-fn calcNext(input: []i64, value: i64, target: i64) bool {
+fn calcNextP1(input: []i64, value: i64, target: i64) bool {
     if (input.len == 0) {
         if (value == target) return true;
         return false;
     }
     //Add
     var next_value = value + input[0];
-    if (calcNext(input[1..], next_value, target)) return true;
+    if (calcNextP1(input[1..], next_value, target)) return true;
 
     next_value = value * input[0];
-    if (calcNext(input[1..], next_value, target)) return true;
+    if (calcNextP1(input[1..], next_value, target)) return true;
+
+    return false;
+}
+
+fn calcNextP2(input: []i64, value: i64, target: i64) bool {
+    if (input.len == 0) {
+        if (value == target) return true;
+        return false;
+    }
+    //Add
+    var next_value = value + input[0];
+    if (calcNextP2(input[1..], next_value, target)) return true;
+
+    next_value = value * input[0];
+    if (calcNextP2(input[1..], next_value, target)) return true;
 
     var mult: i64 = 10;
-    while (input[0]>mult) {
+    while (input[0]>=mult) {
         mult *= 10;
     }
     next_value = value * mult + input[0];
-    if (calcNext(input[1..], next_value, target)) return true;
+    if (calcNextP2(input[1..], next_value, target)) return true;
 
     return false;
 }
@@ -88,7 +84,7 @@ pub fn task2(_: Allocator, input: *std.ArrayList([]const u8)) aoc.TaskErrors!i64
         }
 
         ind -= 2; //Length of local array
-        if (calcNext(factors[1..ind], factors[0], target)) total += target;
+        if (calcNextP2(factors[1..ind], factors[0], target)) total += target;
     }
 
 
