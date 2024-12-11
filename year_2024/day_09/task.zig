3c0d56bf4a84413d507548953d3123a860c68113
Edward Stables <edward.stables1198@gmail.com>
2024-12-11 00:18:17 +0000

    d9 p2

--- year_2024/day_09/task.zig
+++ year_2024/day_09/task.zig
@@ -50,6 +50,83 @@ pub fn task1(_: Allocator, input: *std.ArrayList([]const u8)) aoc.TaskErrors!i64
     return total;
 }
 
-pub fn task2(_: Allocator, _: *std.ArrayList([]const u8)) aoc.TaskErrors!i64 {
-    return 0;
+const Entry = struct {
+    space: bool = false,
+    id: u32 = 0,
+    moved: bool = false,
+    size: u8 = 0,
+};
+
+pub fn task2(alloc: Allocator, input: *std.ArrayList([]const u8)) aoc.TaskErrors!i64 {
+    var disk = try std.ArrayList(Entry).initCapacity(alloc, input.items[0].len);
+    defer disk.deinit();
+
+    for (input.items[0], 0..) |char, ind| {
+        if (ind % 2 == 1) {
+            try disk.append(.{.space=true, .size=char-48, .moved=true});
+        } else {
+            try disk.append(.{.id=@intCast(ind/2), .size=char-48});
+        }
+    }
+    //for (disk.items) |item| {
+    //    for (0..item.size) |_| {
+    //        std.debug.print("{u}", .{if (item.space) '.' else (@as(u8,@intCast(48 + item.id)))});
+    //    }
+    //}
+    //std.debug.print("\n", .{});
+
+
+    var final_index = disk.items.len-1;
+    while (final_index > 0) {
+        var i = &disk.items[final_index];
+        if (i.moved) {
+            final_index -= 1;
+            continue;
+        }
+
+        var ind: u32 = 0;
+        var moved = false;
+        while (ind < final_index) : (ind += 1) {
+            var target = &disk.items[ind];
+            if (!target.space) continue;
+            if (target.size < i.size) continue;
+
+
+            moved = true;
+            const remaining_space = target.size - i.size;
+            target.space = false;
+            target.id = i.id;
+            target.size = i.size;
+
+            i.space = true;
+
+            if (remaining_space > 0) {
+                try disk.insert(ind+1, .{.space = true, .size = remaining_space, .moved = true});
+            }
+            break;
+        }
+
+        //no space
+        if (!moved) {
+            i.moved = true;
+        }
+        final_index -= 1;
+    }
+
+    var total: i64 = 0;
+    var mul: i64 = 0;
+    
+    for (disk.items) |item| {
+        if (item.space) {
+            mul += item.size;
+            continue;
+        }
+        for (0..item.size) |_| {
+            total += item.id * mul;
+            mul += 1;
+        }
+    }
+
+
+    return total;
 }
