a76a53bee51deb54e3019517617b726de4a36d98
Edward Stables <edward.stables1198@gmail.com>
2024-12-23 10:36:31 +0000

    d22 working

--- year_2024/day_22/task.zig
+++ year_2024/day_22/task.zig
@@ -26,19 +26,35 @@ fn bruteforce(start: u64, iterations: u64) u64 {
 }
 
 const hist = struct { _1: i5, _2: i5, _3: i5, _4: i5 };
-const changeProfitPair = struct { change: i5, profit: i5 };
 
-fn changes(alloc: Allocator, start: u64) !std.ArrayList(changeProfitPair) {
-    var hists = try std.ArrayList(changeProfitPair).initCapacity(alloc, 2000);
+fn changes(alloc: Allocator, start: u64) !std.AutoHashMap(hist, u4) {
+    var hists = std.AutoHashMap(hist, u4).init(alloc);
+    var _1: i5 = 0;
+    var _2: i5 = 0;
+    var _3: i5 = 0;
+    var _4: i5 = 0;
     var prev_price: i5 = @intCast(start % 10);
+    var have_hist: u32 = 0;
     var secret = start;
 
-    for (0..2001) |_| {
+    for (0..2000) |_| {
         secret = bruteforce(secret, 1);
         const price: i5 = @intCast(secret % 10);
         const diff: i5 = price - prev_price;
         prev_price = price;
-        try hists.append(.{ .change = diff, .profit = price });
+
+        _4 = _3;
+        _3 = _2;
+        _2 = _1;
+        _1 = diff;
+        have_hist += 1;
+
+        if (have_hist > 4) {
+            const h: hist = .{ ._1 = _1, ._2 = _2, ._3 = _3, ._4 = _4 };
+            if (!hists.contains(h)) {
+                try hists.put(h, @intCast(price));
+            }
+        }
     }
 
     return hists;
@@ -52,43 +68,18 @@ pub fn task1(_: Allocator, input: *std.ArrayList([]const u8)) aoc.TaskErrors!i64
     return total;
 }
 
-fn profitFromHist(all_hists: std.ArrayList(std.ArrayList(changeProfitPair)), test_hist: hist) u64 {
+fn profitFromHist(all_hists: *std.ArrayList(std.AutoHashMap(hist, u4)), test_hist: hist) u64 {
     var total: u64 = 0;
-    var ii: usize = 0;
     for (all_hists.items) |inp| {
-        for (3..inp.items.len) |i| {
-            const this_his: hist = .{
-                ._4 = inp.items[i - 3].change,
-                ._3 = inp.items[i - 2].change,
-                ._2 = inp.items[i - 1].change,
-                ._1 = inp.items[i - 0].change,
-            };
-
-            if (test_hist._1 == this_his._1 and
-                test_hist._2 == this_his._2 and
-                test_hist._3 == this_his._3 and
-                test_hist._4 == this_his._4)
-            {
-                total += @intCast(inp.items[i].profit);
-                //if (test_hist._1 == 0 and
-                //    test_hist._2 == -1 and
-                //    test_hist._3 == 9 and
-                //    test_hist._4 == -9)
-                //{
-                //    std.debug.print("{d}\n", .{total});
-                //}
-                break;
-            }
-        }
-        ii += 1;
+        total += inp.get(test_hist) orelse 0;
     }
     return total;
 }
 
 pub fn task2(alloc: Allocator, input: *std.ArrayList([]const u8)) aoc.TaskErrors!i64 {
-    var all_changes = try std.ArrayList(std.ArrayList(changeProfitPair)).initCapacity(alloc, input.items.len);
+    var all_changes = std.ArrayList(std.AutoHashMap(hist, u4)).init(alloc);
     defer {
-        for (all_changes.items) |c| c.deinit();
+        for (0..all_changes.items.len) |i| all_changes.items[i].deinit();
         all_changes.deinit();
     }
 
@@ -100,28 +91,23 @@ pub fn task2(alloc: Allocator, input: *std.ArrayList([]const u8)) aoc.TaskErrors
     var profits = std.AutoHashMap(hist, u64).init(alloc);
     defer profits.deinit();
 
+    var max_profit: ?u64 = null;
     var best_hist: ?hist = null;
-    var best_profit: i64 = 0;
 
-    for (all_changes.items, 0..) |inputline, progress| {
-        std.debug.print("{d}/{d}\n", .{ progress, all_changes.items.len });
-        for (3..inputline.items.len) |i| {
-            const target: hist = .{
-                ._4 = inputline.items[i - 3].change,
-                ._3 = inputline.items[i - 2].change,
-                ._2 = inputline.items[i - 1].change,
-                ._1 = inputline.items[i - 0].change,
-            };
-            if (profits.contains(target)) continue;
-            const profit = profitFromHist(all_changes, target);
-            try profits.put(target, profit);
-            if (profit > best_profit) {
-                best_hist = target;
-                best_profit = @intCast(profit);
+    for (all_changes.items, 0..) |c, progress| {
+        std.debug.print("Line {d}\n", .{progress});
+        var kit = c.keyIterator();
+        while (kit.next()) |k| {
+            if (profits.contains(k.*)) continue;
+            const p = profitFromHist(&all_changes, k.*);
+            try profits.put(k.*, p);
+            if (p > (max_profit orelse 0)) {
+                max_profit = p;
+                best_hist = k.*;
+                std.debug.print("Best so far {d} with {d},{d},{d},{d}\n", .{ max_profit.?, best_hist.?._4, best_hist.?._3, best_hist.?._2, best_hist.?._1 });
             }
         }
     }
 
-    std.debug.print("Part 2 Solution: {d},{d},{d},{d}\n", .{ best_hist.?._4, best_hist.?._3, best_hist.?._2, best_hist.?._1 });
-    return best_profit;
+    return @intCast(max_profit.?);
 }
