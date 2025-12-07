d412f3fc296b75cddc4230afa81674976af82e34
Edward Stables <edward.stables1198@gmail.com>
2025-12-07 15:50:09 +0000

    d7

---
+++ year_2025/day_07/task.zig
@@ -0,0 +1,63 @@
+const std = @import("std");
+const aoc = @import("../../aoc_util.zig");
+const Allocator = std.mem.Allocator;
+
+pub fn task1(_: Allocator, input: *std.ArrayList([]const u8)) aoc.TaskErrors!i64 {
+    var beam: u160 = 0;
+    const one: u160 = 1;
+    for (input.items[0], 0..) |c, i| {
+        if (c == 'S') {
+            beam |= one << @intCast(i);
+            break;
+        }
+    }
+
+    var count: i64 = 0;
+    for (input.items[1..]) |line| {
+        for (line, 0..) |c, i| {
+            const shift: u8 = @intCast(i);
+            if (c == '^' and (beam & (one << shift))!=0)  {
+                beam ^= (one << shift);
+                beam |= (one << shift-1);
+                beam |= (one << shift+1);
+                count += 1;
+            }
+        }
+    }
+
+    return count;
+}
+
+pub fn task2(alloc: Allocator, input: *std.ArrayList([]const u8)) aoc.TaskErrors!i64 {
+    var beam = try alloc.alloc(u64, input.items[0].len); defer alloc.free(beam);
+    var beam_next = try alloc.alloc(u64, input.items[0].len); defer alloc.free(beam_next);
+    for (input.items[0], 0..) |c, i| {
+        beam[i] = 0;
+        beam_next[i] = 0;
+        if (c == 'S') {
+            beam[i] = 1;
+        }
+    }
+
+    var count: i64 = 1;
+    for (input.items[1..]) |line| {
+        for (line, 0..) |c, i| {
+            if (c == '^' and beam[i] != 0)  {
+                beam_next[i] = 0;
+                beam_next[i-1] += beam[i];
+                beam_next[i+1] += beam[i];
+                count += @intCast(beam[i]);
+            } else {
+                beam_next[i] += beam[i];
+            }
+
+            beam[i] = 0;
+        }
+        const tmp = beam;
+        beam = beam_next;
+        beam_next = tmp;
+    }
+
+    return count;
+}
+
